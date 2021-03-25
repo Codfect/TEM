4591205d27281f9a89b82a48d425f9799d114f9d
Oliver <oliverjgodoy@gmail.com>
2021-03-25 17:31:52 -0300

    eslint sets and configs

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ checkMessage: 'Hello Server'});
-})
+app.get('/', (request, response) => response.json({ checkMessage: 'Hello Server' }));
 
 app.listen(3333, () => {
   console.log('Start Server On Development');
-})
+});
