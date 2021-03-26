73238f33aa0096a7c5002d5b1448d63fcb3e467d
Oliver <oliverjgodoy@gmail.com>
2021-03-26 18:05:26 -0300

    routes and import typescript resolve

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
-import('./database');
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ checkMessage: 'Hello Server' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Start Server On Development');
