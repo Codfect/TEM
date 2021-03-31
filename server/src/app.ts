5a88b5d752ed8a6563e205d4b8d43b5df2550a1e
Oliver <oliverjgodoy@gmail.com>
2021-03-31 16:34:59 -0300

    cors problems resolve

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/class.routes';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
