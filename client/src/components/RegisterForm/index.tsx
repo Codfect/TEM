ab3f8a8835f341b9801b20842bc94b3d42d41958
Oliver <oliverjgodoy@gmail.com>
2021-03-31 14:58:54 -0300

    create api.post

--- client/src/components/RegisterForm/index.tsx
+++ client/src/components/RegisterForm/index.tsx
@@ -1,19 +1,57 @@
-import { Container, Content } from "./styles";
 import { Link } from 'react-router-dom';
+import { api } from '../../services/api';
+import { FormEvent, useState } from 'react';
+
+import { Container, Content } from "./styles";
 import { FiArrowLeft } from 'react-icons/fi';
 
 export function RegisterForm() {
+
+  const [name, setName] = useState('');
+  const [doctor, setDoctor] = useState('');
+  const [date, setDate] = useState('');
+
+  async function handleRegister(event: FormEvent) {
+    event.preventDefault();
+
+    const result = {
+      name,
+      doctor,
+      date,
+    };
+    try {
+      const response = await api.post('/class', result);
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return(
     <Container>
-      <Content>
+      <Content onSubmit={handleRegister}>
         <Link to="/" >
           <FiArrowLeft size={21} color="#fff" />
         </Link>
         <h2>Novo Agendamento</h2>
-        <input placeholder="Nome"/>
-        <input placeholder="Doutor"/>
-        <input type="calendar" placeholder="Data" />
-        <button type="button">
+        <input 
+          placeholder="Nome"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+
+        <input
+          placeholder="Doutor"
+          value={doctor}
+          onChange={event => setDoctor(event.target.value)}
+        />
+
+        <input 
+          placeholder="Data" 
+          value={date}
+          onChange={event => setDate(event.target.value)}
+        />
+
+        <button type="submit">
           Salvar
         </button>
       </Content>
